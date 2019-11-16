31421f36004472cfa1c4608b55061b75f6bdf4d3
Jeffrey Newman <jeff@newman.me>
2019-11-16 15:54:33 -0600

    trim conda update script

--- conda_update.sh
+++ conda_update.sh
@@ -8,10 +8,10 @@ export CONDA_PKG_VERSION="0.1.17"
 conda build $CONDA_RECIPE_DIR --output-folder $CONDA_BUILD_DIR -c jpn --python 3.6
 conda build $CONDA_RECIPE_DIR --output-folder $CONDA_BUILD_DIR -c jpn --python 3.7
 
-anaconda upload $CONDA_BUILD_DIR/osx-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2
+# anaconda upload $CONDA_BUILD_DIR/osx-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2
 
-conda convert --platform win-64 $CONDA_BUILD_DIR/osx-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2 -o $CONDA_BUILD_DIR
-anaconda upload $CONDA_BUILD_DIR/win-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2
+# conda convert --platform win-64 $CONDA_BUILD_DIR/osx-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2 -o $CONDA_BUILD_DIR
+# anaconda upload $CONDA_BUILD_DIR/win-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2
 
-conda convert --platform linux-64 $CONDA_BUILD_DIR/osx-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2 -o $CONDA_BUILD_DIR
-anaconda upload $CONDA_BUILD_DIR/linux-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2
+# conda convert --platform linux-64 $CONDA_BUILD_DIR/osx-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2 -o $CONDA_BUILD_DIR
+# anaconda upload $CONDA_BUILD_DIR/linux-64/$CONDA_PKG_NAME-$CONDA_PKG_VERSION-*.tar.bz2
