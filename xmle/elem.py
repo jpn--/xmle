cb26d1eb100821be892bbf21cc5617d00587331c
Jeff Newman <jeff@newman.me>
2020-02-05 10:52:23 -0600

    remove frameon from kwargs in savefig
    should stop the deprecation warning from matplotlib

--- xmle/elem.py
+++ xmle/elem.py
@@ -255,10 +255,18 @@ class Elem(Element):
 		if 'GRAPHWIDTH' in kwargs and 'GRAPHHEIGHT' not in kwargs:
 			kwargs['GRAPHHEIGHT'] = kwargs['GRAPHWIDTH'] * .67
 		imgbuffer = BytesIO()
-		fig.savefig(imgbuffer, dpi=None, facecolor='w', edgecolor='w',
-						   orientation='portrait', papertype=None, format=format,
-						   transparent=transparent, bbox_inches=bbox_inches, pad_inches=0.1,
-						   frameon=None)
+		fig.savefig(
+			imgbuffer,
+			dpi=None,
+			facecolor='w',
+			edgecolor='w',
+			orientation='portrait',
+			papertype=None,
+			format=format,
+			transparent=transparent,
+			bbox_inches=bbox_inches,
+			pad_inches=0.1,
+		)
 		x = cls("div", {'class': classname})
 		try:
 			x << cls.from_any(imgbuffer.getvalue())
